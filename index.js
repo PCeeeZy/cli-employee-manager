50c912e70e52909105b53d92b5573f063bba9b6e
Paul Cwik <paul@cwik.us>
2022-09-26 11:50:35 -0500

    downgraded inquirer to utilize prompt

--- index.js
+++ index.js
@@ -22,3 +22,7 @@ THEN I am prompted to enter the intern’s name, ID, email, and school, and I am
 WHEN I decide to finish building my team
 THEN I exit the application, and the HTML is generated
 */
+
+const init = async () => {
+    const answer = await inquirer.prompt()
+}
