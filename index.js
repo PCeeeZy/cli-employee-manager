da22ad86c5c6063cf3595de20a9fe6c33fda8506
Paul Cwik <paul@cwik.us>
2022-09-26 11:36:30 -0500

    employee documented

--- index.js
+++ index.js
@@ -1,3 +1,8 @@
+const inquirer = require('inquirer');
+const Engineer = require('./lib/Engineer');
+const Intern = require('./lib/Intern');
+const Manager = require('./lib/Manager');
+
 /*
 GIVEN a command-line application that accepts user input
 WHEN I am prompted for my team members and their information
